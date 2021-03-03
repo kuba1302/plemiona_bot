cf1f4c58ddfec627c811c358eb497449e355cc6a
kuba1302 <kuba1302@gmail.com>
2021-03-03 23:21:05 +0100

    building

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ from time import sleep
 import random
 from password import login, password
 
-class Plemiona_bot:
+class PlemionaBot:
     def __init__(self):
         path = r"C:\Users\Admin\Desktop\chromedriver.exe"
         self.driver = webdriver.Chrome(path)
+
+    def close_popup(self):
+        self.driver.find_element_by_xpath('/html/body/div[11]/div[1]/div/a').click()
+
     def login(self):
         self.driver.get("https://www.plemiona.pl")
         # input login and password
@@ -17,4 +21,24 @@ class Plemiona_bot:
         # select active world
         self.driver.find_element_by_class_name('world_button_active').click()
         # close popup
-        self.driver.find_element_by_xpath('/html/body/div[11]/div[1]/div/a').click()
+        self.close_popup()
+
+    def village_view(self):
+        self.bot.driver.find_element_by_xpath('/html/body/table/tbody/tr[2]/td[2]/table[1]'
+                                              '/tbody/tr/td[1]/table/tbody/tr[1]/td/table/tbody/tr/td[1]/a').click()
+
+    def ratusz_view(self):
+        self.bot.driver.find_element_by_xpath('/html/body/table/tbody/tr[2]/td[2]/table[2]/tbody/tr/td/table/tbody/tr/td/table/'
+                                              'tbody/tr/td/table/tbody/tr/td[1]/div[1]/div/table/tbody/tr[1]/td/a').click()
+    def ratusz_build(self):
+        self.bot.driver.find_element_by_xpath('/html/body/table/tbody/tr[2]/td[2]/table[2]/tbody/tr/td/table/tbody'
+                                              '/tr/td/table/tbody/tr/td/div/table[1]/tbody/tr[2]/td[7]/a[2]').click()
+    def tartak_build(self):
+        self.driver.find_element_by_xpath('/html/body/table/tbody/tr[2]/td[2]/table[2]/tbody/tr/td/table/tbody/tr/td/table/tbody/'
+                                          'tr/td/div/table[1]/tbody/tr[2]/td[7]/a[2]').click()
+    def cegielnia_build(self):
+        self.driver.find_element_by_xpath('/html/body/table/tbody/tr[2]/td[2]/table[2]/tbody/tr/td/table/tbody/'
+                                          'tr/td/table/tbody/tr/td/div/table[1]/tbody/tr[7]/td[7]/a[2]').click()
+    def huta_build(self):
+        self.driver.find_element_by_xpath('/html/body/table/tbody/tr[2]/td[2]/table[2]/tbody/tr/td/table/tbody/'
+                                          'tr/td/table/tbody/tr/td/div/table[1]/tbody/tr[8]/td[7]/a[2]')
